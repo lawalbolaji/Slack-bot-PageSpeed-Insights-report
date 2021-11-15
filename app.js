9b62bf7df80de33bed539685c685ac120d1aa439
lawal bolaji <lawalabdulrasheedbolaji@gmail.com>
2021-11-15 10:47:53 +0100

    configure slack client

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
 
-// configure slack app
+// Initialize app in socket mode
 const app = new App({
     token: process.env['BOT_USER_TOKEN'],
-    signingSecret: process.env['BOT_SIGNING_SECRET']
+    signingSecret: process.env['BOT_SIGNING_SECRET'],
+    socketMode: true,
+    appToken: process.env['SOCKET_MODE_TOKEN'],
+
+    // expose port for possible OAuth
+    port: process.env['PORT'] || 3000
 });
 
-async function run(){
+// listen for messages containing hello
+app.message('hello psi', async ({message, say}) => {
+    
+    await say({
+        blocks: [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `Hi <@${message.user}>`
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": `Click Me`
+                    },
+                    "action_id": "button_click"
+                }
+            }
+        ],
+        text: `Hi <@${message.user}>`
+    });
+});
+
+app.action("button_click", async ({body, ack, say}) => {
+
+    await ack();
+    await say(`<@${body.user.id}> clicked the button`);
+
+});
+
+// run app
+(async () => {
 
     // start app
-    await app.start(process.env['PORT'] || 3000);
+    await app.start();
 
     console.log('⚡️ Bolt app is running!');
-};
-
-run();
+})();
